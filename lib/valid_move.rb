cfd5443a80beb1085740c3ea21d84772b8a29343
David Petnick <dpetnick89@gmail.com>
2020-05-22 22:39:46 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,16 @@
 # code your #valid_move? method here
+def valid_move
 
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?
+  input = board[1..9]
+  if board[0] == "X" || board[0] == "O"
+    return true
+  end
+
+  if board[0] == " " || board[0] == "" || board[0] == nil
+    return false
+   end
+end
