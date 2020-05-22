8e2b80ccf066f812fd7d656c94428082444fd8fe
David Petnick <dpetnick89@gmail.com>
2020-05-22 23:17:55 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -12,9 +12,7 @@ def position_taken?(board, index)
   input = board[1..9]
   if board[index] == " " || board[index] == "" || board[index] == nil
     false
-  end
-
-  if board[index] == "X" || board[index] == "O"
+  else board[index] == "X" || board[index] == "O"
     true
   end
 end
