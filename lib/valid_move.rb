94a4416a5aec561d65ab5c4e5eb486d965628e1a
David Petnick <dpetnick89@gmail.com>
2020-05-22 23:10:45 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,8 +2,8 @@
 def valid_move?(board, index)
   if index.between?(0,8) && position_taken?(board, index) == false
     true
-  else
-    nil
+  elsif position_taken?(board, index) == true
+    false
   end
 end
 
